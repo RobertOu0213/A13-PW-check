31cbf8ad8a46761ea16bf2af0ca9b1474631b263
RobertOu0213 <starshining12345@gmail.com>
2023-05-17 22:52:36 +0800

    feat: add models

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const hbs = require("express-handlebars");
+const PW = require("./models/pw");
 
 const port = 3000;
 require("./config/mongoose");
@@ -8,7 +9,10 @@ require("./config/mongoose");
 app.engine("handlebars", hbs.engine());
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) =>
+ res.render("index", { PW }));
+
+ 
 app.listen(port, () => {
   console.log(`listening on http://localhost:${port}`);
 });
