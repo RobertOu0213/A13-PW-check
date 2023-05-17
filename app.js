1216c961ec871876e6a43e65561ecafaf2970879
RobertOu0213 <starshining12345@gmail.com>
2023-05-17 23:43:39 +0800

    feat: if email & password, redirect to '/'

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ require("./config/mongoose");
 app.engine("handlebars", hbs.engine());
 app.set("view engine", "handlebars");
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => res.render("index"));
+
+app.post("/login", (req, res) => {
+  if (!req.body.email || !req.body.password) return res.redirect("/");
+
+  PW.create(req.body)
+    .then(() => res.redirect("/"))
+    .catch((error) => console.log(error));
+});
 
 app.listen(port, () => {
   console.log(`listening on http://localhost:${port}`);
