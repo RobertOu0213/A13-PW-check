68cdd88cde35d4991a3ea67fbb68e10ab3f696e0
RobertOu0213 <starshining12345@gmail.com>
2023-05-17 22:31:13 +0800

    project init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const hbs = require("express-handlebars");
+
+const port = 3000;
+require("./config/mongoose");
+
+app.engine("handlebars", hbs.engine());
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => res.render("index"));
+app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`);
+});
---
+++ config/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const db = mongoose.connection;
+db.on("error", () => console.log("mongodb error!"));
+db.once("open", () => console.log("mongodb connected"));
+
+module.exports = db;
